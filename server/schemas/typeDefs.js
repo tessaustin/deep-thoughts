35c4ee2f3bfe329b08e12c52a6e04cd8f6106f66
Tess Austin <tessaustin92@gmail.com>
2021-06-09 09:57:14 -0600

    added mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -29,22 +29,26 @@ const typeDefs = gql`
     username: String
   }
 
-  type Query {
-  users: [User]
-  user(username: String!): User
-  thoughts(username: String): [Thought]
-  thought(_id: ID!): Thought
-  }
-
   type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  addThought(thoughtText: String!): Thought
+  addReaction(thoughtId: ID!, reactionBody: String!): Thought
+  addFriend(friendId: ID!): User
   }
 
   type Auth {
   token: ID!
   user: User
   }
+
+  type Query {
+  me: User
+  users: [User]
+  user(username: String!): User
+  thoughts(username: String): [Thought]
+  thought(_id: ID!): Thought
+}
 
 `;
 
