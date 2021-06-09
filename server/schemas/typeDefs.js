617af66061c1021dafd17fd9df218a379eddae13
Tess Austin <tessaustin92@gmail.com>
2021-06-09 09:37:13 -0600

    authorized users with JWTs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -34,7 +34,18 @@ const typeDefs = gql`
   user(username: String!): User
   thoughts(username: String): [Thought]
   thought(_id: ID!): Thought
-}
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+
+  type Auth {
+  token: ID!
+  user: User
+  }
+
 `;
 
 // export the typeDefs
