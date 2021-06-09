b2d21b5dbc5d70799a9eb4d4546da97b62258167
Tess Austin <tessaustin92@gmail.com>
2021-06-08 21:25:26 -0600

    reamining query Type definition and resolvers built

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,6 +4,15 @@ const { gql } = require('apollo-server-express');
 // create our typeDefs
 const typeDefs = gql`
 
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
   type Thought {
     _id: ID
     thoughtText: String
@@ -21,8 +30,11 @@ const typeDefs = gql`
   }
 
   type Query {
-    thoughts(username: String): [Thought]
-  }
+  users: [User]
+  user(username: String!): User
+  thoughts(username: String): [Thought]
+  thought(_id: ID!): Thought
+}
 `;
 
 // export the typeDefs
