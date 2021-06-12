160b9918e1c877b8f0b3a0c460244a9d09aca37a
Tess Austin <tessaustin92@gmail.com>
2021-06-12 16:15:12 -0600

    implemented user-based quiries

--- client/src/App.js
+++ client/src/App.js
@@ -14,19 +14,17 @@ import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
 });
 
 function App() {
@@ -54,3 +52,4 @@ function App() {
 }
 
 export default App;
+
